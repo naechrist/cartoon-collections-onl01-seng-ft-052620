70662097ad0edc95328bd651e1cc68fe5c50b765
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-31 15:15:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-array.find do |cheese|
+array.include? do |cheese|
 cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
 end
 end
