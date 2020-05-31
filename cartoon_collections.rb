47623a8782e1d6bffff7fc4d2ba53d3c64d04ed6
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-31 15:14:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+ array.each_with_index do |name, index|
+ puts "#{index + 1}. #{name}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |word|
+    word.capitalize + "!"
+  end
+end
+Array
+
+def long_planeteer_calls(array)
+ array.any? do |word|
+   word.length > 4
+ end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def find_the_cheese(array)
+array.include? do |cheese|
+cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
 end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end
+find_the_cheese(array)
